3e9a4280bd5b3c17ccd9823e67169e1fad606b68
Vane789 <anlly.cardona@genstudents.org>
2024-06-06 09:51:19 -0500

    FIX - Se arregla errores modal

--- services/modal.js
+++ services/modal.js
@@ -60,10 +60,11 @@ function productoModal(producto) {
     descripcion.textContent = producto.descripcion;
     descripcion.classList.add("descripcion-modal")
     cerrar.textContent = "X"
-    image.src = producto.imagen || "http://127.0.0.1:5501/assets/img/img-modal.jpg"
+    image.src = producto.imagen || "http://127.0.0.1:5500/assets/img/img-modal.jpg" // cambiar esta parte !!IMPORTANT
     image.title = producto.nombre
     image.classList.add("image_modal")
     div_info.classList.add("info_modal")
+    // mensaje.innerHTML = 'Si deseas personalizar tu torta da clic <a href="#" >aquí</a>.';
 
     cerrar.classList.add("cerrar-modal")
 
@@ -72,6 +73,7 @@ function productoModal(producto) {
     div_info.appendChild(descripcion)
     div_info.appendChild(cerrar)
     div_info.appendChild(formulario)
+    // div_info.appendChild(mensaje)
 
     informacion_modal.appendChild(div_image)
     informacion_modal.appendChild(div_info)
