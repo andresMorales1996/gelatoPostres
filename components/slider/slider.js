36667391cedb97123de76f93da0ea0494bd40ac8
velennidelalahoz <velenni.delahoz@genstudents.org>
2024-07-10 20:49:20 -0500

    sebe ajustes de pagina productos

--- components/slider/slider.js
+++ components/slider/slider.js
@@ -1,27 +1,90 @@
-  //Mobile
+//  //Mobile
+// const sliderInner = document.querySelector('.slider-inner');
+// const sliderItems = document.querySelectorAll('.slider-item');
+// const prevButton = document.querySelector('.prev');
+// const nextButton = document.querySelector('.next');
+// const buttons = document.querySelectorAll('.desp-btn');
+// let currentIndex = 0;
+
+
+
+
+
+// buttons.forEach(button => {
+//   button.addEventListener('click', function() {
+//     const descText = this.parentElement.nextElementSibling;
+//     const img = this.querySelector('img');
+    
+//     if (descText.style.display === 'none' || descText.style.display === '') {
+//       descText.style.display = 'block';
+//       img.src = '/assets/svg/down-ico.png'; 
+//     } else {
+//       descText.style.display = 'none';
+//       img.src = '/assets/svg/up-ico.png'; 
+//     }
+//   });
+// });
+
+
+// function updateSliderPosition() {
+//     const offset = -currentIndex * 100;
+//     sliderInner.style.transform = `translateX(${offset}%)`;
+// }
+
+// prevButton.addEventListener('click', () => {
+//      if (currentIndex > 0) {
+//          currentIndex--;
+//          updateSliderPosition();
+//      }
+//  });
+
+// nextButton.addEventListener('click', () => {
+//     if (currentIndex < sliderItems.length - 1) {
+//         currentIndex++;
+//         updateSliderPosition();
+//     }
+// });
+
+// updateSliderPosition();
+
+// //Desktop
+// currentIndex = 2;
+// const items = document.querySelectorAll('.slider-item');
+// const dots = document.querySelectorAll('.dots span');
+
+// const updateCarousel = () => {
+//   items.forEach((item, index) => {
+//     item.classList.remove('active');
+//     if (index === currentIndex) {
+//       item.classList.add('active');
+//     }
+//   });
+
+//   dots.forEach((dot, index) => {
+//     dot.classList.remove('active');
+//     if (index === currentIndex) {
+//       dot.classList.add('active');
+//     }
+//   });
+// };
+
+// items.forEach((item, index) => {
+//   item.addEventListener('click', () => {
+//     currentIndex = index;
+//     updateCarousel();
+//   });
+// });
+
+// // Inicializar carrusel
+// updateCarousel();
+
+
+//Mobile
 const sliderInner = document.querySelector('.slider-inner');
 const sliderItems = document.querySelectorAll('.slider-item');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
-const buttons = document.querySelectorAll('.desp-btn');
 let currentIndex = 0;
-
-
-buttons.forEach(button => {
-  button.addEventListener('click', function() {
-    const descText = this.parentElement.nextElementSibling;
-    const img = this.querySelector('img');
-    
-    if (descText.style.display === 'none' || descText.style.display === '') {
-      descText.style.display = 'block';
-      img.src = '/assets/svg/down-ico.png'; 
-    } else {
-      descText.style.display = 'none';
-      img.src = '/assets/svg/up-ico.png'; 
-    }
-  });
-});
-
 
 function updateSliderPosition() {
     const offset = -currentIndex * 100;
@@ -29,11 +92,11 @@ function updateSliderPosition() {
 }
 
 prevButton.addEventListener('click', () => {
-     if (currentIndex > 0) {
-         currentIndex--;
-         updateSliderPosition();
-     }
- });
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateSliderPosition();
+    }
+});
 
 nextButton.addEventListener('click', () => {
     if (currentIndex < sliderItems.length - 1) {
@@ -65,12 +128,12 @@ const updateCarousel = () => {
   });
 };
 
-items.forEach((item, index) => {
-  item.addEventListener('click', () => {
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
     currentIndex = index;
     updateCarousel();
   });
 });
 
-// Inicializar carrusel
+// Initialize the carousel
 updateCarousel();
