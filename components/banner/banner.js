e03070a42f87983ffe6d994d7d1382d53455cbbf
Vane789 <anlly.cardona@genstudents.org>
2024-07-12 16:05:26 -0500

    se arregla banner 2

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -5,10 +5,10 @@ const swiper_banners = new Swiper('.swiper-banners', {
     el: '.swiper-pagination',
     clickable: true,
   },  
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 8000,
+  //   disableOnInteraction: false,
+  // },
   speed: 1000
 });
 
