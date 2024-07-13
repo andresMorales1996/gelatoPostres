4f5b879fd86c936edaf434b74ad5dd0da2e51066
Vane789 <anlly.cardona@genstudents.org>
2024-07-12 23:39:35 -0500

    se pone linea de código banner

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -5,11 +5,11 @@ const swiper_banners = new Swiper('.swiper-banners', {
     el: '.swiper-pagination',
     clickable: true,
   },  
-  // autoplay: {
-  //   delay: 8000,
-  //   disableOnInteraction: false,
-  // },
-  speed: 1000
+  autoplay: {
+    delay: 8000,
+    disableOnInteraction: false,
+  },
+  speed: 2000
 });
 
 const swiper_testimonio = new Swiper('.swiper-testimonio', {
