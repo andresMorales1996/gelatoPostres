0b105d3aad68dc427ae2cd9e442419cbb98f805c
AlissonAlvarez <alisson.alvarez@genstudents.org>
2024-07-15 01:23:40 -0500

    Actualización de módulos

--- app/rellenos/api-rellenos.js
+++ app/rellenos/api-rellenos.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const rellenosTableBody = document.querySelector("#rellenosTabla tbody");
 
-  function obtenerRellenos() {
-    fetch("http://localhost:8080/relleno")
-      .then(response => response.json())
-      .then(data => {
+  function allRellenos() {
+    fetch("http://localhost:8080/rellenos/v1/allRellenos")
+      .then((response) => response.json())
+      .then((data) => {
         rellenosTableBody.innerHTML = "";
 
-        data.forEach(relleno => {
+        data.forEach((relleno) => {
           const row = document.createElement("tr");
 
           const cellId = document.createElement("td");
@@ -15,45 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
           row.appendChild(cellId);
 
           const cellRelleno = document.createElement("td");
-          cellRelleno.textContent = relleno.relleno;
+          cellRelleno.textContent = relleno.nombre_relleno;
           row.appendChild(cellRelleno);
 
-          const cellOpciones = document.createElement("td");
+          const cellPrecio = document.createElement("td");
+          cellPrecio.textContent = relleno.precio_relleno;
+          row.appendChild(cellPrecio);
 
-          const botonActualizar = document.createElement("button");
-          botonActualizar.textContent = "Actualizar";
-          botonActualizar.className = "boton-actualizar";
-          botonActualizar.addEventListener("click", function() {
-            // Lógica para actualizar el relleno
-            actualizarRelleno(relleno.id_relleno);
-          });
-          cellOpciones.appendChild(botonActualizar);
-
-          const botonEliminar = document.createElement("button");
-          botonEliminar.textContent = "Eliminar";
-          botonEliminar.className = "boton-eliminar";
-          botonEliminar.addEventListener("click", function() {
-            // Lógica para eliminar el relleno
-            eliminarRelleno(relleno.id_relleno);
-          });
-          cellOpciones.appendChild(botonEliminar);
-
-          row.appendChild(cellOpciones);
           rellenosTableBody.appendChild(row);
         });
       })
-      .catch(error => console.error("Error al obtener los rellenos:", error));
+      .catch((error) => console.error("Error al obtener los rellenos:", error));
   }
 
-  obtenerRellenos();
-
-  function actualizarRelleno(id) {
-    // Lógica para actualizar el relleno con el id proporcionado
-    console.log(`Actualizar relleno con ID: ${id}`);
-  }
-
-  function eliminarRelleno(id) {
-    // Lógica para eliminar el relleno con el id proporcionado
-    console.log(`Eliminar relleno con ID: ${id}`);
-  }
+  allRellenos();
 });
