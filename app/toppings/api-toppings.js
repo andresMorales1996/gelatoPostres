a2a862ece41690553cca6d90f307e210bbc0361d
AlissonAlvarez <alisson.alvarez@genstudents.org>
2024-07-15 14:42:36 -0500

    Actualización

--- app/toppings/api-toppings.js
+++ app/toppings/api-toppings.js
@@ -1,33 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
   const toppingsTableBody = document.querySelector("#toppingsTabla tbody");
 
-
   function allToppings() {
-      fetch("http://localhost:8080/toppings/v1/allToppings")
-          .then(response => response.json())
-          .then(data => {
-            toppingsTableBody.innerHTML = "";
+    fetch("http://localhost:8080/toppings/v1/allToppings")
+      .then(response => response.json())
+      .then(data => {
+        toppingsTableBody.innerHTML = "";
 
-              data.forEach(topping => {
-                  const row = document.createElement("tr");
+        data.forEach(topping => {
+          const row = document.createElement("tr");
 
-                  const cellId = document.createElement("td");
-                  cellId.textContent = topping.id_topping;
-                  row.appendChild(cellId);
+          const cellId = document.createElement("td");
+          cellId.textContent = topping.id_topping;
+          row.appendChild(cellId);
 
-                  const cellNombre = document.createElement("td");
-                  cellNombre.textContent = topping.nombre_topping;
-                  row.appendChild(cellNombre);
+          const cellNombre = document.createElement("td");
+          cellNombre.textContent = topping.nombre_topping;
+          row.appendChild(cellNombre);
 
-                  const cellPrecio = document.createElement("td");
-                  cellPrecio.textContent = topping.precio_topping;
-                  row.appendChild(cellPrecio);
+          const cellPrecio = document.createElement("td");
+          cellPrecio.textContent = topping.precio_topping;
+          row.appendChild(cellPrecio);
 
-                  toppingsTableBody.appendChild(row);
-              });
-          })
-          .catch(error => console.error("Error al obtener los toppings:", error));
+          const cellOpciones = document.createElement("td");
+
+          const botonActualizar = document.createElement("button");
+          botonActualizar.classList.add("btn", "btn-actualizar");
+          botonActualizar.innerHTML = '<i class="fas fa-edit"></i>';
+          botonActualizar.addEventListener("click", function() {
+          });
+
+          const botonEliminar = document.createElement("button");
+          botonEliminar.classList.add("btn", "btn-eliminar");
+          botonEliminar.innerHTML = '<i class="fas fa-trash-alt"></i>';
+          botonEliminar.addEventListener("click", function() {
+          });
+
+          cellOpciones.appendChild(botonActualizar);
+          cellOpciones.appendChild(botonEliminar);
+          row.appendChild(cellOpciones);
+
+          toppingsTableBody.appendChild(row);
+        });
+      })
+      .catch(error => console.error("Error al obtener los toppings:", error));
   }
 
   allToppings();
 });
+
